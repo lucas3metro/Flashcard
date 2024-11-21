d2bdc896b453d6a993d88543886331ea274bab35
lucas3metro <00001095331759sp@al.educacao.sp.gov.br>
2024-11-21 13:41:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 9 x 9?',
+    '9 x 9 é 81'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlim'
+)
+
+criaCartao(
+    'esporte',
+    'Em que ano o Neymar Jr ganhou a Champions league?',
+    'Neymar Jr ganhou a Champions league em 2015'
+)
+
+criaCartao(
+    'linguagens',
+    'Como se diz bom dia em inglês?',
+    'Bom dia em ingles é good morning '
+)
